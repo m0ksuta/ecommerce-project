798644ad1716f23737aeef0fff15a5812c83dbc4
m0ksuta <olhovskii.m@mail.ru>
2021-01-26 17:00:15 +0300

    Working cart

--- shop/urls.py
+++ shop/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('<slug:category_slug>/<slug:product_slug>', views.product, name='products_detail'),
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='products_detail'),
+    path('cart', views.cart_detail, name='cart_detail'),
+    path('cart/add/<int:product_id>', views.add_cart, name='add_cart'),
+    path('cart/remove/<int:product_id>', views.cart_remove, name='cart_remove'),
+    path('cart/delete/<int:product_id>', views.cart_remove_product, name='cart_remove_product'),
+
 ]
